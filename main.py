003b66408c2c3405d136f644fac74afabb6015d9
Dmitry Chirin <chirin.dima@gmail.com>
2021-03-19 21:18:00 +0100

    added test (Entity) object

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from Entity import *
 player = Entity(10, 10, '@', 'Player', 'white', True)
 orc = Entity(15, 10, 'o', 'Orc', 'green', True)
 sword = Entity(15, 15, '/', 'Sword', 'light_blue', False)
+test = Entity()
 
 entities = []
 entities.append(player)
 entities.append(orc)
 entities.append(sword)
+entities.append(test)
 
 while True:
     for entity in entities:
