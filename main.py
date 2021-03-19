1d989d4ecfb24f1dae6c910d69b7cdd2fccc3aa7
Dmitry Chirin <chirin.dima@gmail.com>
2021-03-19 21:16:37 +0100

    added while loop and movement (with input())

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ from Entity import *
 
 player = Entity(10, 10, '@', 'Player', 'white', True)
 orc = Entity(15, 10, 'o', 'Orc', 'green', True)
-sword = Entity(15, 15, '', 'Sword', 'light_blue', False)
+sword = Entity(15, 15, '/', 'Sword', 'light_blue', False)
 
 entities = []
 entities.append(player)
 entities.append(orc)
 entities.append(sword)
 
-for entity in entities:
-    print('X:{}     Y:{}    Char:{}     Name:{}     Color:{}    Blocks Movement:{}'.format(
-        entity.x, entity.y, entity.char, entity.name, entity.color, entity.blocksMovement))
+while True:
+    for entity in entities:
+        print('X:{}     Y:{}    Char:{}     Name:{}     Color:{}    Blocks Movement:{}'.format(
+            entity.x, entity.y, entity.char, entity.name, entity.color, entity.blocksMovement))
+
+    inp = input('>>> ').strip().lower()
+    for letter in inp:
+        if letter == 'w':
+            player.y -= 1
+        if letter == 's':
+            player.y += 1
+        if letter == 'a':
+            player.x -= 1
+        if letter == 'd':
+            player.x += 1
