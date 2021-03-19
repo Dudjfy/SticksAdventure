0e2caa86f948a4449ce528c577d8093821a31732
Dmitry Chirin <chirin.dima@gmail.com>
2021-03-19 20:55:33 +0100

    added orc object

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from Entity import *
 
 player = Entity(10, 10, '@', 'Player', 'blue', True)
+orc = Entity(15, 10, 'o', 'Orc', 'white', True)
+
+
 
 entities = []
 entities.append(player)
+entities.append(orc)
 
 for entity in entities:
     print('X:{}     Y:{}    Char:{}     Name:{}     Color:{}    Blocks Movement:{}'.format(
